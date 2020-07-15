fd285d5ef9beb1be4dd6f8b68b96719d71073422
Rezo Megrelidze <rezomegrelidze@gmail.com>
2020-07-16 02:23:11 +0300

    Added Pieces and changed the board class

--- ConsoleChessGame/Program.cs
+++ ConsoleChessGame/Program.cs
@@ -23,41 +23,104 @@ namespace ConsoleChessGame
 
         public ChessGame()
         {
-            AddPieces();
+            Board = new Board();
         }
 
-        private void AddPieces()
-        {
-            Pieces = new List<Piece>();
+        
 
-            // White Pawns
+        public void PrintGame()
+        {
             for (int i = 0; i < 8; i++)
             {
-                Pieces.Add(new Pawn(i, i,PieceColor.White));
-            }
-        }
+                for (int j = 0; j < 8; j++)
+                {
+                    
+                }
 
-        public List<Piece> Pieces { get; set; }
+                Console.WriteLine();
+            }
+
+        }
     }
 
     public class Pawn : Piece
     {
 
-        public override IEnumerable<Move> AvailableMoves( ){
+        public override IEnumerable<Move> AvailableMoves( )
+        {
             throw new NotImplementedException();
         }
 
-        public Pawn(int rank, int file, PieceColor color) : base(rank, file, color)
+        public Pawn(Square square, PieceColor color) : base(square, color)
         {
+        }
+    }
+
+    public class Rook : Piece
+    {
+        public Rook(Square square, PieceColor color) : base(square, color)
+        {
+        }
+
+        public override IEnumerable<Move> AvailableMoves()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Bishop : Piece
+    {
+        public Bishop(Square square, PieceColor color) : base(square, color)
+        {
+        }
+
+        public override IEnumerable<Move> AvailableMoves()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Knight : Piece
+    {
+        public Knight(Square square, PieceColor color) : base(square, color)
+        {
+        }
+
+        public override IEnumerable<Move> AvailableMoves()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class King : Piece
+    {
+        public King(Square square, PieceColor color) : base(square, color)
+        {
+        }
+
+        public override IEnumerable<Move> AvailableMoves()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Queen : Piece
+    {
+        public Queen(Square square, PieceColor color) : base(square, color)
+        {
+        }
+
+        public override IEnumerable<Move> AvailableMoves()
+        {
+            throw new NotImplementedException();
         }
     }
 
     public abstract class Piece
     {
-
-        public Piece(int rank, int file, PieceColor color)
+        protected Piece(Square square, PieceColor color)
         {
-            CurrentSquare = new Square(rank, file);
+            CurrentSquare = square;
             Color = color;
         }
         public Square CurrentSquare { get; set; }
@@ -77,10 +140,28 @@ namespace ConsoleChessGame
 
     public struct Square
     {
-        public Square(int rank, int file) => (Rank, File) = (rank, file);
+        public Square(int rank, int file,SquareColor color) => (Rank, File,Color) = (rank, file,color);
+
+
+        public SquareColor Color { get; }
 
         public int Rank { get; }
         public int File { get; }
+
+        public static bool operator ==(Square square1, Square square2)
+        {
+            return square1.Rank == square2.Rank && square1.File == square2.File;
+        }
+
+        public static bool operator !=(Square square1, Square square2)
+        {
+            return !(square1 == square2);
+        }
+    }
+
+    public enum SquareColor
+    {
+        White,Black
     }
 
     public enum PieceColor
@@ -99,10 +180,44 @@ namespace ConsoleChessGame
 
         private void BuildBoard()
         {
-            for(int i = 0;i < 8;i++)
-                for(int j = 0;j < 8;j++)
-                    Squares[i,j] = new Square(i,j);
+            var squareColor = SquareColor.Black;
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Squares[i, j] = new Square(i, j, squareColor);
+                    squareColor = squareColor == SquareColor.Black ? SquareColor.White : SquareColor.Black;
+                }
+            }
         }
+
+        private void AddPieces()
+        {
+            Pieces = new List<Piece>();
+
+            // Pawns6,
+            for (int i = 0; i < 8; i++)
+            {
+                Pieces.AddRange(new[] { new Pawn(Squares[1, i], PieceColor.White), new Pawn(Squares[6, i], PieceColor.Black) });
+            }
+            //  Pieces
+            var rooks = new[]   {new Rook(Squares[0, 0], PieceColor.White), new Rook(Squares[0, 7], PieceColor.White)
+                ,   new Rook(Squares[7, 0], PieceColor.Black), new Rook(Squares[7, 7], PieceColor.Black) };
+            var knights = new[] {new Knight(Squares[0, 1], PieceColor.White), new Knight(Squares[0, 6], PieceColor.White),
+                new Knight(Squares[7, 1], PieceColor.Black), new Knight(Squares[7, 6], PieceColor.Black) };
+            var bishops = new[]
+            {
+                new Bishop(Squares[0, 2], PieceColor.White), new Bishop(Squares[0, 5], PieceColor.White),
+                new Bishop(Squares[7, 2], PieceColor.Black), new Bishop(Squares[7, 5], PieceColor.Black)
+            };
+
+            var kings = new[] { new King(Squares[0, 4], PieceColor.White), new King(Squares[7, 4], PieceColor.Black) };
+            var queens = new[] { new Queen(Squares[0, 3], PieceColor.White), new Queen(Squares[7, 3], PieceColor.Black) };
+
+            Pieces.AddRange((new Piece[][] { rooks, knights, bishops, kings, queens }).SelectMany(l => l));
+        }
+
+        public List<Piece> Pieces { get; set; }
 
         public Square this[int rank, int file] => Squares[rank, file];
     }
